0d8a699a535b85c8df2de385fc77b34b92546e48
Anders Ringqvist <anders.ringqvist@gmail.com>
2011-03-21 22:36:58 +0100

    Changes ie legacy script.

--- recipes/js/ie-legacy.js
+++ recipes/js/ie-legacy.js
@@ -1,8 +1,5 @@
 // make ie support "elements unsupported by design" (e.g. <abbr>)
 
 (function (i, tags) {
-	while (
-		document.createElement(tags[i]),
-		tags[++i]
-	);
+	while (document.createElement(tags[i++]), tags[i]);
 }(0, 'abbr, article, aside, section'.replace(/\s+/g, '').split(',')));
